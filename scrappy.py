796a21a5fed92f236c12378ae4a5be2ebcefe604
pavelexpertov <pavelm01@hotmail.co.uk>
2021-02-08 22:26:03 +0000

    update initial interface

--- scrappy.py
+++ scrappy.py
@@ -4,11 +4,21 @@ import click
 def cli():
     pass
 
-@click.command()
-def generate_document():
-    print('hey man')
+@cli.command()
+@click.argument('job_url')
+def get_metadata(job_url):
+    '''Get metadata from a single URL source'''
+    print(job_url)
 
-cli.add_command(generate_document)
+@cli.command()
+@click.argument('urls_file_path')
+@click.argument('document_file_path')
+def get_metadatas(urls_file_path, document_file_path):
+    '''Save parsed job metadata that's described in a file to a specific document'''
+    with open(urls_file_path) as file_obj:
+        url_list = [line.strip() for line in file_obj.readlines()]
+
+    click.echo(url_list)
 
 if __name__ == "__main__":
     cli()
