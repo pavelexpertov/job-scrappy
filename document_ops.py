bff00ddcead0955c22b7ab561a404ce74e88260e
pavelexpertov <pavelm01@hotmail.co.uk>
2021-02-09 22:37:31 +0000

    Turn pages into landscape mode

--- document_ops.py
+++ document_ops.py
@@ -5,6 +5,12 @@ def generate_document(content_list, file_path):
     organised_key_list = ['roles_and_responsibilities', 'required_stuff', 'preferred_stuff', 'passed_url']
     document = docx.Document()
 
+    # Setting up Landscape mode for a page
+    section = document.sections[0]
+    section.orientation = docx.enum.section.WD_ORIENT.LANDSCAPE
+    new_height, new_width = section.page_width, section.page_height
+    section.page_height, section.page_width = new_height, new_width
+
     for content in content_list:
         document.add_heading(f"{content['title']} -- {content['company']}", level=4)
         document.add_paragraph(content['introduction'])
